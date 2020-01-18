2786b1635f50a9c1455b2fda905e5e5db0c0cc42
Gavin Williams <gamichwi@gmail.com>
2020-01-18 19:04:55 +1100

    created places-routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const placesRoutes = require('./routes/places-routes')
+
 const app = express();
 
 app.listen(5000);
