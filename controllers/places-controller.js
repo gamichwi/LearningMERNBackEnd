c4b5d56cd479c87f34b7f775bb72b86b673ef805
Gavin Williams <gamichwi@gmail.com>
2020-01-19 14:21:42 +1100

    added create place route. Installed uuid to handle unique ids.

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -43,5 +43,21 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 };
 
+const createPlace = (req, res, next) => {
+    const { title, description, coordinates, address, creator } = req.body;
+    const createdPlace = {
+        title,
+        description,
+        location: coordinates,
+        address,
+        creator
+    }
+
+    DUMMY_PLACES.push(createdPlace);
+
+    res.status(201).json({place: createdPlace}) // Succesfully created code.
+}
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
