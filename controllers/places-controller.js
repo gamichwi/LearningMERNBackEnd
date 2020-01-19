ec7ce9c0af5ead547f5426c1c30d2d0b642bada2
Gavin Williams <gamichwi@gmail.com>
2020-01-19 20:29:17 +1100

    added CRUD routes for places.

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,8 +1,8 @@
-const uuid = require('uuid/v4');
+const uuid = require("uuid/v4");
 
-const HttpError = require('../models/http-error')
+const HttpError = require("../models/http-error");
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: "p1",
     title: "Scary castle",
@@ -45,21 +45,43 @@ const getPlaceByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
-    const { title, description, coordinates, address, creator } = req.body;
-    const createdPlace = {
-        id: uuid(),
-        title,
-        description,
-        location: coordinates,
-        address,
-        creator
-    }
+  const { title, description, coordinates, address, creator } = req.body;
+  const createdPlace = {
+    id: uuid(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator
+  };
 
-    DUMMY_PLACES.push(createdPlace);
+  DUMMY_PLACES.push(createdPlace);
 
-    res.status(201).json({place: createdPlace}) // Succesfully created code.
-}
+  res.status(201).json({ place: createdPlace }); // Succesfully created code.
+};
+
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) }; //copy of place
+  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({ place: updatedPlace });
+};
+
+const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
+  res.status(200).json({ message: 'Deleted place.' })
+};
 
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
