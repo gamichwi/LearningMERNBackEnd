a6054fadf9a426ec4f7378a571fd83f9244158a4
Gavin Williams <gamichwi@gmail.com>
2020-01-19 13:35:06 +1100

    added error handling middleware for places-routes and others.

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -21,6 +21,12 @@ router.get('/:pid', (req, res, next)=> {
     const place = DUMMY_PLACES.find(p => {
         return p.id === placeId;
     })
+
+    if (!place) {
+        const error = new Error('Could not find a place for the provided place id.')
+        error.code = 404
+        return next(error);
+    }
     res.json({ place }); // => {place:place}
 });
 
@@ -29,6 +35,11 @@ router.get('/user/:uid', (req, res, next) => {
     const place = DUMMY_PLACES.find(p => {
         return p.creator === userId;
     })
+    if (!place) {
+        const error = new Error('Could not find a place for the provided user id.')
+        error.code = 404
+        return next(error);
+    }
     res.json({ place })
 })
 
