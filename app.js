e895d86c6d100125ff68dab86dfa983db4d482d5
Gavin Williams <gamichwi@gmail.com>
2020-01-20 22:35:40 +1100

    created user routes (Signup, Login, Get Users)

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const placesRoutes = require('./routes/places-routes')
-const httpError = require('./models/http-error')
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/api/places', placesRoutes); // => /api/places/...
+app.use("/api/places", placesRoutes); // => /api/places/...
+app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
-    return new httpError('Could not find this route', 404);
+  return new HttpError("Could not find this route", 404);
 }); //Only reached if we have a request that hasn't had a response before
 
 //error handling middleware
 app.use((error, req, res, next) => {
-    if (res.headersSent) {
-        return next(error);
-    }
-    res.status(error.code || 500);
-    res.json({message: error.message || 'An unknown error occured!'});
-})
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occured!" });
+});
 
 app.listen(5000);
